1eb4a5953688c4f349f2c732fc7bc37ae4f73766
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2022-02-27 20:28:16 -0600

    Add data structures

--- functions.py
+++ functions.py
@@ -1,3 +1,34 @@
+from typing import NamedTuple
 
+
+class Post(NamedTuple):
+    datecreated: str
+    post: str
+
+
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class LinkedList:
+   def __init__(self):
+      self.headval = None
+
+class User(NamedTuple):
+    username: str
+    email: str
+    password: str
+    interests: list
+    posts: LinkedList
+
+
+
+list1 = LinkedList()
+list1.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+# Link first Node to second node
+list1.headval.nextval = e2
 
 
