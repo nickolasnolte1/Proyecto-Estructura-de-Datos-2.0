65306308c3adb2b5de0fe0390140a0f6b53d2672
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2022-02-27 21:23:17 -0600

    condensed

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+from distutils.log import debug
 from flask import Flask, render_template, request, url_for, redirect
 from flask.wrappers import Request
 from jinja2 import Template, FileSystemLoader, Environment
+from sympy import true
 from functions import crear_usuario
 
-users=[]
 app = Flask(__name__)
+users=[]
 
 @app.route('/' , methods = ["GET", "POST"])
 def homepage():
@@ -15,27 +17,12 @@ def homepage():
         confirm=request.form['confirm']
         if password==confirm:
             users=crear_usuario(users, username, email, password)
-            return "si se pudo"
-        else:
-            return "error"
+            return redirect('http://localhost:8000/categories', code=302)
     return render_template('signup.html')
 
 @app.route('/categories')
 def categories():
     return render_template('categories.html')
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    username = request.form['username']
-    email=request.form['email']
-    password=request.form['password']
-    confirm=request.form['confirm']
-    if password==confirm:
-        users=crear_usuario(users, username, email, password)
-        return "si se pudo"
-    else:
-        return "error"
-
 
 
 if __name__ == "__main__":
