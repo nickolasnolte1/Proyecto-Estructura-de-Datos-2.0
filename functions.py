37a68c9edf5e6fbdfdfdd149ce59bc36f0314ea1
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2022-03-06 23:08:00 -0600

    Funciones

--- functions.py
+++ functions.py
@@ -50,7 +50,7 @@ def crear_usuario(username,i, email, password):
     a=User(i,username, email, password, interests)
     i+=1
     users.append(a)
-    return users, i-1 
+    return users, i 
 
 def agregar_intereses(users, i, interests):
     a=users[i]
